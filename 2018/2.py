17e77af5700936f5b8db2434fe228a660fa80140
Freddie <freddie.mao@gmail.com>
2022-01-06 21:19:45 +0000

    2018 q2

--- 2018/2.py
+++ 2018/2.py
@@ -1,3 +1,27 @@
+# Remember for question 2 to test many inputs and see if any of them result in errors
+# Debugging is a key part of this question
+
 n, w = input().split()
 n = int(n)
 
+# This creates the decode wheel from n
+def decoder(n):
+    second = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    out = []
+    ptr = 0
+    while len(second) > 0:
+        ptr += n
+        ptr %= len(second)
+        out.append(second.pop(ptr))
+    return out
+
+wheel = decoder(n-1)
+
+print("".join(wheel[0:6]))
+
+# This encodes the text
+out = ""
+for i in range(len(w)):
+    out += wheel[(i + ord(w[i])-65) % 26]
+
+print(out)
