ba2f364016c1798ede3c5c830bebdd93ad0fb9de
freddie <freddie.mao@gmail.com>
2022-01-10 15:59:04 +0000

    partial 2008 q2

---
+++ 2008/2.py
@@ -0,0 +1,52 @@
+n = int(input())
+message = input()
+
+# Turn the first rotor every time, turn the second rotor every 4
+# Must go in cycles of 16
+n %= 16
+
+# All of the values are decremented, negative values go to 3
+def rotate(rotor):
+    for i in range(4):
+        rotor[i] = rotor[i] - 1 if rotor[i] else 3
+    return rotor
+
+# This sets up the rotors
+rotor1 = [0,3,1,2]
+rotor2 = [0,2,1,3]
+reflector = [3,2,1,0]
+
+# This does all of the moves before encoding
+for i in range(n):
+    rotor1 = rotate(rotor1)
+    if i % 4 == 3:
+        rotor2 = rotate(rotor2)
+
+# This will encode a character according to the current rotors
+def encode(letter):
+    pos = ord(letter)-65
+    pos = rotor1[pos]
+    pos = rotor2[pos]
+    pos = reflector[pos]
+    for i in range(4):
+        if rotor2[i] == pos:
+            pos = i
+            break
+    for i in range(4):
+        if rotor1[i] == pos:
+            pos = i
+            break
+    return chr(pos+65)
+
+out = ""
+for i in range(len(message)):
+    out += encode(message[i])
+    # This keeps rotating the wheels
+    n += 1
+    rotor1 = rotate(rotor1)
+    if not n % 4:
+        rotor2 = rotate(rotor2)
+
+print(out)
+
+# Currently with the sample case
